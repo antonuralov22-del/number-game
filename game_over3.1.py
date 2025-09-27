f200cc37f32aea5d0a135bbe6a79379a4f44ad7d
ANTON URALOV <antonuralov22@gmail.com>
2025-09-27 16:38:21 +0300

    Update game_over3.1.py

--- game_over3.1.py
+++ game_over3.1.py
@@ -1,11 +1,4 @@
-def game_core_v3(number: int = 1) -> int:
-    """
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
 
-    Returns:
-        int: Число попыток
-    """
 import random
 
 def game_core_v3(number: int = 1) -> int:
@@ -24,13 +17,39 @@ def game_core_v3(number: int = 1) -> int:
 
     while low <= high:
         attempts += 1
-        guess = (low + high) // 2  # Методом бинарных алгоритмов берем середину диапазона
+        guess = (low + high) // 2  # Берем середину диапазона
 
         if guess == number:
-            return attempts  # Угаданное число
+            return attempts  # Число угадано!
         elif guess < number:
             low = guess + 1  # Сужаем диапазон снизу
         else:
             high = guess - 1  # Сужаем диапазон сверху
 
     return attempts
+
+# Функция для проверки алгоритма
+def score_game(game_core) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        game_core ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    random.seed(1)  # Фиксируем RANDOM SEED для воспроизводимости
+    random_array = [random.randint(1, 101) for _ in range(1000)]
+
+    for number in random_array:
+        count_ls.append(game_core(number))
+
+    score = int(sum(count_ls) / len(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
+
+# Запуск проверки
+if __name__ == "__main__":
+    score_game(game_core_v3)
+
